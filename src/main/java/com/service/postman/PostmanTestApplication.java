b69cbac208a78263b1f496ee1255823d10328fb5
28yashverma <28.yashverma@gmail.com>
2024-04-21 11:17:25 +0530

    initial commit

---
+++ src/main/java/com/service/postman/PostmanTestApplication.java
@@ -0,0 +1,13 @@
+package com.service.postman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostmanTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostmanTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/service/postman/PostmanTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.service.postman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostmanTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
